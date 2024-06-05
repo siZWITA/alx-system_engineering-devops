6d27699116b4f66db5a3d40ccf9efc1fec7a9e22
siZWITA <mourad.elmohib@gmail.com>
2024-06-04 18:31:10 -0700

    API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+file of number of subscribers method
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    number of subscribers method
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data', {}).get('subscribers', 0)
+    else:
+        return 0
